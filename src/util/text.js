c878368148642c9a4ed302f1d1e18c42a3c96e3e
Nicolas Marino <nicolasmarinoparra@gmail.com>
2020-06-02 04:25:46 +0300

    create costume class for text

---
+++ src/util/text.js
@@ -0,0 +1,19 @@
+class Text {
+    constructor(context,x,y,string,style,origin) {
+    this.context = context
+    this.x = x
+    this.y = y
+    this.text = string        
+    this.style = this.initStyle(style)
+
+
+    }
+
+    createText() {
+
+    }
+
+    destroy() {
+
+    }
+}
