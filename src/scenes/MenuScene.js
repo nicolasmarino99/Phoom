8c4ea6eec22c9e59e771a25b5179949743723ca9
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-07-01 17:26:21 +0300

    change paths for deploy

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -4,12 +4,12 @@ import { Align } from '../util/align';
 import Button from './ui/Button';
 import rainImg from '../assets/ui/rain/rain.png';
 import rainJSON from '../assets/ui/rain/rain.json';
-//import fontImg from '../assets/fonts/menu/font.png';
-//import fontFnt from '../assets/fonts/menu/font.fnt';
+import fontImg from '../assets/fonts/menu/font.png';
+import fontFnt from '../assets/fonts/menu/font.fnt';
 import farImg from '../assets/ui/background/far.png';
 import sandImg from '../assets/ui/background/sand.png';
 import foregroundmergedImg from '../assets/ui/background/foreground-merged.png';
-//import menuMusic from './src/assets/music/menu/menuMusic.ogg';
+import menuMusic from './src/assets/music/menu/menuMusic.ogg';
 import img7 from '../assets/ui/buttons/PNG/shiny/7.png';
 import img7shiny from '../assets/ui/buttons/PNG/shiny/7shiny.png';
 import soundOnImg from '../assets/ui/buttons/sound/soundOn.png';
@@ -23,15 +23,15 @@ export default class MenuScene extends Phaser.Scene {
     this.load.atlas('rain', rainImg, rainJSON);
     this.load.bitmapFont(
       'font',
-      './src/assets/fonts/menu/font.png',
-      './src/assets/fonts/menu/font.fnt',
+      fontImg
+      fontFnt
     );
     // Load paralax layers
     this.load.image('far', farImg);
     this.load.image('sand', sandImg);
     this.load.image('foreground-merged', foregroundmergedImg);
     // Load buttons
-    this.load.audio('menuMusic', ['./src/assets/music/menu/menuMusic.ogg']);
+    this.load.audio('menuMusic', [menuMusic]);
     this.load.image('blueButton1', img7);
     this.load.image('blueButton2', img7shiny);
     this.load.image('soundOn', soundOnImg);
